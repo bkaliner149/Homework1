d502ad38676e02739c6a5d1210aa56035b773b73
Brett Kaliner <brett.kaliner149@gmail.com>
2021-09-10 11:48:19 -0400

    Edit 2 to homework 1

--- Homework1.py
+++ Homework1.py
@@ -1,10 +1,11 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 #Data for plotting
-x = (0, 2*np.pi, .01)
-y1 = np.sin
-y2 = np.cos
+x = np.arange(0, 2*np.pi, .1)
+y1 = np.sin(x)
+y2 = np.cos(x)
 
 #Plot
-plt.plot(x, y1, y2)
+plt.plot(x,y1,x,y2)
+plt.show()
